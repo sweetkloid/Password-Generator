2e5716f263efa7d8d450757e5557f2bff0fbfab6
Sami <coolsweetsami@gmail.com>
2023-03-30 09:26:44 -0400

    added to js start

--- assets/css/script.js
+++ assets/css/script.js
@@ -1,16 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var generatePassword= prompt("Hello please enter a password.");
 
+
+var passPromt=function(){
+ let user= prompt("Please enter your password lenght: 8-128", "8-128");
+ if (!user){
+  return;
+ }
+}
+
+  
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-  
+ 
 }
-
-// Add event listener to generate button
+ // Add event listener to generate button
 //
 generateBtn.addEventListener("click", writePassword);
