cff66bc6786c90041100f7b5621f76a6865bb221
Sami2 <coolsweetsami@gmail.com>
2023-03-30 20:02:09 -0400

    updates to JS and readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Password-Generator
 Challenge 3
+
+## Description
+
+## Usage
+
+Linked here is the URL that take you to the Github repo for this webpage: [Github] (https://github.com/sweetkloid/Password-Generator).
+
+Here is the final webpage: [Image of Webpage](assets/images/Untitled_%20Mar%2030%2C%202023%207_53%20PM.webm).
+
+## Credits
+Thanks to bootcamp classmates, instructor, and TA's. They helped make this project possible.
+
+## License
+none
--- assets/css/script.js
+++ assets/css/script.js
@@ -1,22 +1,58 @@
 // Assignment Code
+var characterLength=8;
+var choiceArr=[];
+const alphabet = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", ];
+const lowerCase =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const specialCharacters= ["!", "@", "$", "&", "*"];
+
+
 var generateBtn = document.querySelector("#generate");
 
-
-var passPromt=function(){
- let user= prompt("Please enter your password lenght: 8-128", "8-128");
- if (!user){
-  return;
- }
-}
-
+function getPrompts(){
+  choiceArr= [];
+  characterLength= parseInt(prompt("How many characters would you like your password to be: 8-128?"));
   
+  if(isNaN(characterLength)|| characterLength<8|| characterLength>128){
+    alert("Please enter a number between 8-128.");
+    return false;
+  }
+
+  if (confirm("Would you like to add lowercase letters?")){
+    choiceArr = choiceArr.concat(lowerCase);
+  }
+  if (confirm("Would you like to add uppercase letters?")){
+    choiceArr = choiceArr.concat(alphabet);
+  }
+  if (confirm("Would you like to add special characters?")){
+    choiceArr = choiceArr.concat(specialCharacters);
+  }
+  if (confirm("Would you like to add numbers?")){
+    choiceArr = choiceArr.concat(numbers);
+  }
+return true;
+}
+//  Add event listener to generate button
+
+generateBtn.addEventListener("click", writePassword);
+ 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var correctPrompts =getPrompts();
   var passwordText = document.querySelector("#password");
-  passwordText.value = password;
- 
+  if(correctPrompts){
+    var newPassword = generatePassword();
+    passwordText.value = newPassword;
+  } else {
+    passwordText.value = "";
+  }
+} 
+
+function generatePassword(){
+var password="";
+for(var i=0; i< characterLength; i++){
+var randomIndex= Math.floor(Math.random() * choiceArr.length);
+password= password + choiceArr[randomIndex];
 }
- // Add event listener to generate button
-//
-generateBtn.addEventListener("click", writePassword);
+return password;
+}
